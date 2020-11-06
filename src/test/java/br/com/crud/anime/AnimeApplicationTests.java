da7334c440af000dd21d7f855689e38b5f49b7ea
grimmjhow <felipe.cife@gmail.com>
2020-11-06 08:25:14 -0300

    first project commit

---
+++ src/test/java/br/com/crud/anime/AnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crud.anime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
